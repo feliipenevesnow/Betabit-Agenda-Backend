7cfdb1e991efda8c2f78a3c53a69d880f27dd7e9
Felipe Neves <fneves@1500th.com>
2025-11-06 17:36:55 -0300

    Altera cors

--- config/cors.php
+++ config/cors.php
@@ -6,11 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-        'https://betabit-agenda-frontend.vercel.app', 
-    ],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
